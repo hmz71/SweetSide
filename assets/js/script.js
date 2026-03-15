dfd8dad8560fd529b671ae852d535ec4b6b90c4d
hmz71 <aouraghe.hamza@gmail.com>
2026-03-15 13:10:27 +0100

    feat: add scroll effect on navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,3 +16,13 @@ const observer = new IntersectionObserver(entries => {
 cards.forEach(card => {
   observer.observe(card);
 });
+
+const header = document.querySelector('.header');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 50) {
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
+});
